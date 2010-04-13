0fad06148d46fa20a0a775fd15aede201ba855a5
Hatter <krater@narod.ru>
2010-04-13 06:43:29 -0700

    tabs - > spaces

--- models.py
+++ models.py
@@ -6,13 +6,13 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
 class Task(db.Model):
-	owner = db.UserProperty()
-	content = db.StringProperty(multiline=True)
-	date = db.DateTimePropery(auto_now_add=True)
-	notification_date = db.DateTimeProperty()
-	status = db.ReferenceColumn(Status)
+    owner = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimePropery(auto_now_add=True)
+    notification_date = db.DateTimeProperty()
+    status = db.ReferenceColumn(Status)
 
 class Status(db.Model):
-	owner = db.UserProperty()
-	name = db.StrtingProperty()
+    owner = db.UserProperty()
+    name = db.StrtingProperty()
 	
