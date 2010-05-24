5062e298c13cb55383f515838b6c7f4bf5cc65d1
Hatter <krater@narod.ru>
2010-05-24 15:25:27 +0600

    mods

--- main.py
+++ main.py
@@ -15,15 +15,17 @@
 # limitations under the License.
 #
 
-
+from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
-
+import os
 
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
-    self.response.out.write('Hello world!')
+    template_values={'context_text':'this is context text'}
+    path = os.path.join(os.path.dirname(__file__), 'templates', 'index.html')
+    self.response.out.write(template.render(path, template_values))
 
 
 def main():
